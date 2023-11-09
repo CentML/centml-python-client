79fd0a3d5bdd424db5a5ca1ba48d84196408e655
destefy <stefan.delasa@gmail.com>
2023-11-09 17:13:03 -0500

    double init fix for docker

--- centml/__init__.py
+++ centml/__init__.py
@@ -3,4 +3,5 @@ import torch._dynamo
 from .compiler import backend
 
 # Register centml compiler backend to torch dynamo
-torch._dynamo.register_backend(compiler_fn=backend.centml_dynamo_backend, name="centml")
+if "centml" not in torch._dynamo.list_backends():
+    torch._dynamo.register_backend(compiler_fn=backend.centml_dynamo_backend, name="centml")
