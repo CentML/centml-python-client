b546190949c003fa3f04c2316f822b4ccbbabd41
destefy <stefan.delasa@gmail.com>
2023-11-10 11:55:34 -0500

    config file fix

--- centml/compiler/config.py
+++ centml/compiler/config.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config:
-    TIMEOUT = 3
+    TIMEOUT = 10
     MAX_RETRIES = 3
-    COMPILING_SLEEP_TIME = 2
+    COMPILING_SLEEP_TIME = 15
 
     CACHE_PATH = os.getenv("CENTML_CACHE_DIR", default=os.path.expanduser("~/.cache/centml"))
     SERVER_IP = os.getenv("CENTML_SERVER_IP", default="0.0.0.0")
