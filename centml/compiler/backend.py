165e54fa20a4e92b29c8cad3939a02dcd033f063
destefy <stefan.delasa@gmail.com>
2023-11-23 17:18:35 -0500

    handling and logging exceptions in top level init

--- centml/compiler/backend.py
+++ centml/compiler/backend.py
@@ -3,6 +3,7 @@ import pickle
 import hashlib
 import time
 import tempfile
+import logging
 from http import HTTPStatus
 from typing import List, Callable
 import requests
@@ -18,15 +19,19 @@ base_path = os.path.join(config_instance.CACHE_PATH, "compiler")
 os.makedirs(base_path, exist_ok=True)
 server_url = f"http://{config_instance.SERVER_IP}:{config_instance.SERVER_PORT}"
 
+logger = logging.getLogger(__name__)
+
 
 class Runner:
     def __init__(self, module: torch.fx.GraphModule, inputs: List[torch.Tensor]):
         self._module: torch.fx.GraphModule = module
         self._inputs: List[torch.Tensor] = inputs
         self.compiled_forward_function: Callable[[torch.Tensor], tuple] = None
-        self.exception_logs: str = ""
 
-        self.remote_compilation()
+        try:
+            self.remote_compilation()
+        except Exception as e:
+            logger.exception(f"Remote compilation failed with the following exception: \n")
 
     @property
     def module(self):
@@ -36,18 +41,12 @@ class Runner:
     def inputs(self):
         return self._inputs
 
-    def __print_exceptions(self):
-        print("--- ERROR: Remote compilation failed with the following exceptions: ---")
-        print(self.exception_logs, end="")
-        print("--- Using uncompiled forward function ---")
-
     def __get_model_id(self, flow_graph: hidet.FlowGraph) -> str:
         with tempfile.NamedTemporaryFile() as temp_file:
             try:
                 hidet.save_graph(flow_graph, temp_file.name)
             except Exception as e:
-                self.exception_logs += f"Getting model id: failed to save FlowGraph. {e}\n"
-                return None
+                raise Exception(f"Getting model id: failed to save FlowGraph. {e}\n") from e
 
             with open(temp_file.name, "rb") as f:
                 flow_graph_hash = hashlib.md5(f.read()).hexdigest()
@@ -57,10 +56,9 @@ class Runner:
     def __download_model(self, model_id: str) -> CompiledGraph:
         download_response = requests.get(url=f"{server_url}/download/{model_id}", timeout=config_instance.TIMEOUT)
         if download_response.status_code != HTTPStatus.OK:
-            self.exception_logs += (
-                f"Download: request failed, exception from server: {download_response.json().get('detail')}"
+            raise Exception(
+                f"Download: request failed, exception from server:\n{download_response.json().get('detail')}"
             )
-            return None
 
         download_dir = os.path.join(base_path, model_id)
         os.makedirs(download_dir, exist_ok=True)
@@ -77,7 +75,10 @@ class Runner:
             files={"model": pickle.dumps(self.module), "inputs": pickle.dumps(self.inputs)},
             timeout=config_instance.TIMEOUT,
         )
-        return compile_response
+        if compile_response.status_code != HTTPStatus.OK:
+            raise Exception(
+                f"Compile model: request failed, exception from server:\n{compile_response.json().get('detail')}\n"
+            )
 
     def __wait_for_status(self, model_id: str) -> bool:
         tries = 0
@@ -85,8 +86,10 @@ class Runner:
             # get server compilation status
             status_response = requests.get(f"{server_url}/status/{model_id}", timeout=config_instance.TIMEOUT)
             if status_response.status_code != HTTPStatus.OK:
-                self.exception_logs += f"Status check: exception from server: {status_response.json().get('detail')}"
-                return False
+                raise Exception(
+                    f"Status check: request failed, exception from server:\n{status_response.json().get('detail')}"
+                )
+
             status = status_response.json().get("status")
 
             if status == CompilationStatus.DONE.value:
@@ -95,15 +98,12 @@ class Runner:
                 pass
             elif status == CompilationStatus.NOT_FOUND.value:
                 tries += 1
-                compiled_response = self.__compile_model(model_id)
-                if compiled_response.status_code != HTTPStatus.OK:
-                    self.exception_logs += f"{compiled_response.json().get('detail')}\n"
+                self.__compile_model(model_id)
             else:
                 tries += 1
 
             if tries > config_instance.MAX_RETRIES:
-                self.exception_logs += "Waiting for status: compilation failed too many times.\n"
-                return False
+                raise Exception("Waiting for status: compilation failed too many times.\n")
 
             time.sleep(config_instance.COMPILING_SLEEP_TIME)
 
@@ -112,22 +112,13 @@ class Runner:
         interpreter: Interpreter = hidet.frontend.from_torch(self.module)
         flow_graph, inputs, output_format = get_flow_graph(interpreter, self.inputs)
         model_id = self.__get_model_id(flow_graph)
-        if model_id is None:
-            self.__print_exceptions()
-            return
 
         cgraph_path = os.path.join(base_path, model_id, "cgraph.zip")
         if os.path.isfile(cgraph_path):  # cgraph is saved locally
             cgraph = load_compiled_graph(cgraph_path)
         else:
-            if not self.__wait_for_status(model_id):
-                self.__print_exceptions()
-                return
-
+            self.__wait_for_status(model_id)
             cgraph = self.__download_model(model_id)
-            if cgraph is None:
-                self.__print_exceptions()
-                return
 
         wrapper = get_wrapper(cgraph, inputs, output_format)
         self.compiled_forward_function = wrapper
@@ -135,10 +126,9 @@ class Runner:
     def __call__(self, *args, **kwargs):
         # If model is currently compiling, return the uncompiled forward function
         if not self.compiled_forward_function:
-            return self.module(*args, **kwargs)
+            return self.module.forward(*args, **kwargs)
 
-        forward_function = self.compiled_forward_function(*args)
-        return forward_function
+        return self.compiled_forward_function(*args)
 
 
 def centml_dynamo_backend(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
--- centml/compiler/server.py
+++ centml/compiler/server.py
@@ -1,16 +1,15 @@
 import os
 import pickle
 from http import HTTPStatus
-from typing import List
+import logging
 import uvicorn
-from torch import Tensor
-from torch.fx import GraphModule
-from fastapi import FastAPI, UploadFile, HTTPException, BackgroundTasks
+from fastapi import FastAPI, UploadFile, HTTPException, BackgroundTasks, Response
 from fastapi.responses import FileResponse
 from fastapi.middleware.gzip import GZipMiddleware
 from centml.compiler.server_compilation import hidet_backend_server, storage_path, CompilationStatus, dir_cleanup
 from centml.compiler import config_instance
 
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.add_middleware(GZipMiddleware, minimum_size=1000)
@@ -31,41 +30,52 @@ async def status_handler(model_id: str):
     raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Status check: invalid status state.")
 
 
-def background_compile(model_id: str, tfx_graph: GraphModule, example_inputs: List[Tensor]):
+def background_compile(model_id: str, model: UploadFile, inputs: UploadFile):
     try:
-        # This will save the cgraph to {storage_path}/{model_id}/cgraph.zip
-        hidet_backend_server(tfx_graph, example_inputs, model_id)
-    except Exception:
-        dir_cleanup(model_id)
-
-
-@app.post("/submit/{model_id}")
-async def compile_model_handler(model_id: str, model: UploadFile, inputs: UploadFile, background_task: BackgroundTasks):
-    dir_cleanup(model_id)
-    os.makedirs(os.path.join(storage_path, model_id))
-
-    try:
-        tfx_contents = await model.read()
-        ei_contents = await inputs.read()
+        tfx_contents = model.file.read()
+        ei_contents = inputs.file.read()
     except Exception as e:
+        logger.exception(f"Compilation: error reading serialized content. {e}")
         dir_cleanup(model_id)
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Compilation: error reading serialized content."
-        ) from e
+        return
     finally:
         model.file.close()
+        inputs.file.close()
 
     try:
         tfx_graph = pickle.loads(tfx_contents)
         example_inputs = pickle.loads(ei_contents)
     except Exception as e:
+        logger.exception(f"Compilation: error loading pickled content. {e}")
         dir_cleanup(model_id)
+        return
+
+    try:
+        # This will save the cgraph to {storage_path}/{model_id}/cgraph.zip
+        hidet_backend_server(tfx_graph, example_inputs, model_id)
+    except Exception as e:
+        logger.exception(f"Compilation: error compiling model. {e}")
+        dir_cleanup(model_id)
+
+
+@app.post("/submit/{model_id}")
+async def compile_model_handler(model_id: str, model: UploadFile, inputs: UploadFile, background_task: BackgroundTasks):
+    try:
+        status = (await status_handler(model_id)).get("status")
+    except Exception as e:
         raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Compilation: error loading pickled content."
+            status_code=HTTPStatus.BAD_REQUEST, detail=f"Compilation: error checking status. {e}"
         ) from e
 
-    # perform the compilation in the background after return HTTP.OK to client
-    background_task.add_task(background_compile, model_id, tfx_graph, example_inputs)
+    # Only compile if the model is not compiled or compiling
+    if status != CompilationStatus.NOT_FOUND:
+        return Response(status_code=200)
+
+    # This effectively sets the model's status to COMPILING
+    os.makedirs(os.path.join(storage_path, model_id))
+
+    # perform the compilation in the background and return HTTP.OK to client
+    background_task.add_task(background_compile, model_id, model, inputs)
 
 
 @app.get("/download/{model_id}")
