75f3c129f22ed4f19e13a3cb8e78b3aef0199213
Stefan de Lasa <96326431+destefy@users.noreply.github.com>
2023-12-07 13:21:30 -0500

    Non blocking compilation (#16)
    * non-blocking version that removes uncompiled model form gpu
    * successfully free the memory associated with the uncompiled model after compiled. The gc is not tracking any references to it
    * call gc explicitly to be safe
    * use lock to avoid race condition when switching from uncompiled to compiled

--- centml/compiler/backend.py
+++ centml/compiler/backend.py
@@ -1,19 +1,23 @@
 import os
+import gc
 import pickle
 import hashlib
 import time
 import tempfile
 import logging
+import weakref
+import threading as th
 from http import HTTPStatus
 from typing import List, Callable
 import requests
-import hidet
 import torch
+import hidet
 from hidet.graph.frontend.torch.interpreter import Interpreter
 from hidet.graph.frontend.torch.dynamo_backends import get_flow_graph, get_wrapper
 from hidet.runtime.compiled_graph import load_compiled_graph, CompiledGraph
 from centml.compiler import config_instance
 from centml.compiler.server_compilation import CompilationStatus
+
 
 base_path = os.path.join(config_instance.CACHE_PATH, "compiler")
 os.makedirs(base_path, exist_ok=True)
@@ -24,22 +28,34 @@ logger = logging.getLogger(__name__)
 
 class Runner:
     def __init__(self, module: torch.fx.GraphModule, inputs: List[torch.Tensor]):
-        self._module: torch.fx.GraphModule = module
+        self._module: torch.fx.GraphModule = weakref.ref(module)
         self._inputs: List[torch.Tensor] = inputs
+
         self.compiled_forward_function: Callable[[torch.Tensor], tuple] = None
 
+        self.lock = th.Lock()
+        self.child_thread = th.Thread(target=self.remote_compilation)
         try:
-            self.remote_compilation()
+            self.child_thread.start()
         except:
             logger.exception("Remote compilation failed with the following exception: \n")
 
     @property
     def module(self):
-        return self._module
+        return self._module()
 
     @property
     def inputs(self):
         return self._inputs
+
+    @module.deleter
+    def module(self):
+        self._module().graph.owning_module = None
+        self._module = None
+
+    @inputs.deleter
+    def inputs(self):
+        self._inputs = None
 
     def __get_model_id(self, flow_graph: hidet.FlowGraph) -> str:
         with tempfile.NamedTemporaryFile() as temp_file:
@@ -65,9 +81,8 @@ class Runner:
         download_path = os.path.join(download_dir, "cgraph.zip")
         with open(download_path, "wb") as f:
             f.write(download_response.content)
-        cgraph = load_compiled_graph(download_path)
 
-        return cgraph
+        return load_compiled_graph(download_path)
 
     def __compile_model(self, model_id: str):
         compile_response = requests.post(
@@ -111,8 +126,10 @@ class Runner:
         # start by getting the model_id
         interpreter: Interpreter = hidet.frontend.from_torch(self.module)
         flow_graph, inputs, output_format = get_flow_graph(interpreter, self.inputs)
+
         model_id = self.__get_model_id(flow_graph)
 
+        # check if cgraph is saved locally
         cgraph_path = os.path.join(base_path, model_id, "cgraph.zip")
         if os.path.isfile(cgraph_path):  # cgraph is saved locally
             cgraph = load_compiled_graph(cgraph_path)
@@ -123,10 +140,19 @@ class Runner:
         wrapper = get_wrapper(cgraph, inputs, output_format)
         self.compiled_forward_function = wrapper
 
+        # Let gc free the memory used by the uncompiled model
+        with self.lock:
+            interpreter = None
+            del self.inputs
+            del self.module
+            gc.collect()
+            torch.cuda.empty_cache()
+
     def __call__(self, *args, **kwargs):
         # If model is currently compiling, return the uncompiled forward function
-        if not self.compiled_forward_function:
-            return self.module.forward(*args, **kwargs)
+        with self.lock:
+            if not self.compiled_forward_function:
+                return self.module.forward(*args, **kwargs)
 
         return self.compiled_forward_function(*args)
 
