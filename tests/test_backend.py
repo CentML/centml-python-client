15da4c08d6b622d893d8d422255546e86d427f7c
destefy <stefan.delasa@gmail.com>
2023-12-11 16:13:53 -0500

    Add test for wait_for_status and download_model

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,10 +1,13 @@
+from http import HTTPStatus
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import torch
 from torch.fx import GraphModule
 import hidet
 from hidet.graph.frontend.torch.dynamo_backends import get_flow_graph
 from centml.compiler.backend import Runner
+from centml.compiler.server_compilation import CompilationStatus
+from centml.compiler import config_instance
 
 
 class TestGetModelId(TestCase):
@@ -28,6 +31,7 @@ class TestGetModelId(TestCase):
     def test_none_flow_graph(self, mock_save_graph):
         with self.assertRaises(Exception) as context:
             self.runner._get_model_id(None)
+
         self.assertIn("Getting model id: flow graph is None.", str(context.exception))
         mock_save_graph.assert_not_called()
 
@@ -36,6 +40,7 @@ class TestGetModelId(TestCase):
         mock_save_graph.side_effect = Exception("Test Exception")
         with self.assertRaises(Exception) as context:
             self.runner._get_model_id(self.flow_graph)
+
         self.assertIn("Getting model id: failed to save FlowGraph.", str(context.exception))
         mock_save_graph.assert_called_once()
 
@@ -48,3 +53,106 @@ class TestGetModelId(TestCase):
         model_id1 = self.runner._get_model_id(self.flow_graph)
         model_id2 = self.runner_34._get_model_id(self.flow_graph_34)
         self.assertNotEqual(model_id1, model_id2)
+
+
+class TestDownloadModel(TestCase):
+    @patch('threading.Thread.start')
+    def setUp(self, mock_thread) -> None:
+        model = MagicMock(spec=GraphModule)
+        self.runner = Runner(model, None)
+
+    @patch('os.makedirs')
+    @patch('centml.compiler.backend.requests')
+    def test_failed_download(self, mock_requests, mock_makedirs):
+        # Mock the response from the requests library
+        mock_response = MagicMock()
+        mock_response.status_code = HTTPStatus.NOT_FOUND
+        mock_requests.get.return_value = mock_response
+
+        model_id = "download_fail"
+        with self.assertRaises(Exception) as context:
+            self.runner._download_model(model_id)
+
+        mock_requests.get.assert_called_once()
+        self.assertIn("Download: request failed, exception from server", str(context.exception))
+        mock_makedirs.assert_not_called()
+
+    @patch('os.makedirs')
+    @patch('builtins.open')
+    @patch('centml.compiler.backend.load_compiled_graph')
+    @patch('centml.compiler.backend.requests')
+    def test_successful_download(self, mock_requests, mock_load, mock_open, mock_makedirs):
+        # Mock the response from the requests library
+        mock_response = MagicMock(spec=bytes)
+        mock_response.status_code = HTTPStatus.OK
+        mock_response.content = b'model_data'
+        mock_requests.get.return_value = mock_response
+
+        # Call the _download_model function
+        model_id = "download_success"
+        self.runner._download_model(model_id)
+
+        mock_requests.get.assert_called_once()
+        mock_load.assert_called_once()
+        mock_open.assert_called_once()
+        mock_makedirs.assert_called_once()
+
+
+class TestWaitForStatus(TestCase):
+    @patch('threading.Thread.start')
+    def setUp(self, mock_thread) -> None:
+        model = MagicMock(spec=GraphModule)
+        self.runner = Runner(model, None)
+
+    @patch('centml.compiler.backend.requests')
+    def test_invalid_status(self, mock_requests):
+        mock_response = MagicMock()
+        mock_response.status_code = HTTPStatus.BAD_REQUEST
+        mock_requests.get.return_value = mock_response
+
+        model_id = "invalid_status"
+        with self.assertRaises(Exception) as context:
+            self.runner._wait_for_status(model_id)
+
+        mock_requests.get.assert_called_once()
+        self.assertIn("Status check: request failed, exception from server", str(context.exception))
+
+    @patch('centml.compiler.config.Config.COMPILING_SLEEP_TIME', new=0)
+    @patch('centml.compiler.backend.Runner._compile_model')
+    @patch('centml.compiler.backend.requests')
+    def test_max_tries(self, mock_requests, mock_compile):
+        mock_response = MagicMock()
+        mock_response.status_code = HTTPStatus.OK
+        mock_response.json.return_value = {"status": CompilationStatus.NOT_FOUND.value}
+        mock_requests.get.return_value = mock_response
+
+        model_id = "max_tries"
+        with self.assertRaises(Exception) as context:
+            self.runner._wait_for_status(model_id)
+
+        self.assertEqual(mock_compile.call_count, config_instance.MAX_RETRIES + 1)
+        self.assertIn("Waiting for status: compilation failed too many times", str(context.exception))
+
+    @patch('centml.compiler.config.Config.COMPILING_SLEEP_TIME', new=0)
+    @patch('centml.compiler.backend.requests')
+    def test_wait_on_compilation(self, mock_requests):
+        COMPILATION_STEPS = 10
+        mock_response = MagicMock()
+        mock_response.status_code = HTTPStatus.OK
+        mock_response.json.side_effect = [{"status": CompilationStatus.COMPILING.value}] * COMPILATION_STEPS + [
+            {"status": CompilationStatus.DONE.value}
+        ]
+        mock_requests.get.return_value = mock_response
+
+        model_id = "compilation_done"
+        self.runner._wait_for_status(model_id)
+
+    @patch('centml.compiler.backend.requests')
+    def test_compilation_done(self, mock_requests):
+        mock_response = MagicMock()
+        mock_response.status_code = HTTPStatus.OK
+        mock_response.json.return_value = {"status": CompilationStatus.DONE.value}
+        mock_requests.get.return_value = mock_response
+
+        model_id = "compilation_done"
+        self.runner._wait_for_status(model_id)
