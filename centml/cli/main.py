5b7ce0d0559f52b89934199e104a38eb4cbce5df
Anand J <anandj@cs.toronto.edu>
2023-12-18 17:08:10 -0500

    Change remote compilation server name to "compile-server"

--- centml/cli/main.py
+++ centml/cli/main.py
@@ -5,11 +5,11 @@ from ..compiler import server
 
 def main():
     parser = argparse.ArgumentParser(description="CentML command line tool")
-    parser.add_argument("mode", choices=["server"], type=str)
+    parser.add_argument("mode", choices=["compile-server"], type=str)
 
     args = parser.parse_args()
 
-    if args.mode == "server":
+    if args.mode == "compile-server":
         server.run()
     else:
         raise Exception("Invalid mode")
