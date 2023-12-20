c1b78b50586d1e6a053bf037b19c7552b5f55610
Anand J <anandj@cs.toronto.edu>
2023-12-20 18:50:25 -0500

    Make centml as the parent command
    Add ccluster command under centml

--- centml/cli/main.py
+++ centml/cli/main.py
@@ -5,11 +5,18 @@ from ..compiler import server
 
 def main():
     parser = argparse.ArgumentParser(description="CentML command line tool")
-    parser.add_argument("mode", choices=["compile-server"], type=str)
+    subparser = parser.add_subparsers(help="sub-command help", dest="mode")
+
+    server_parser = subparser.add_parser("server", help="Remote computation server")
+
+    ccompute_parser = subparser.add_parser("cluster", help="CCompute CLI tool")
 
     args = parser.parse_args()
 
-    if args.mode == "compile-server":
+    if args.mode == "server":
         server.run()
+    elif args.mode == "cluster":
+        print(args.mode)
     else:
-        raise Exception("Invalid mode")
+        parser.print_help()
+        parser.exit()
