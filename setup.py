47b502ada9e796884c980cb2cea9c3b743cb2ce1
destefy <stefan.delasa@gmail.com>
2023-12-20 17:45:35 -0500

    Define status_handler return to be able to generate client side usage

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt') as f:
 setup(
     name='centml',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     scripts=['bin/ccompute'],
     install_requires=REQUIRES
 )
