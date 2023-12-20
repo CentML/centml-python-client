05fad93055de5bf70479c75f4e5fc65ce3728d60
destefy <stefan.delasa@gmail.com>
2023-12-20 17:40:11 -0500

    Move model_suite to helpers file

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,5 @@
 import torch
-
+from transformers import BertForPreTraining, AutoTokenizer
 
 @staticmethod
 def get_graph_module_for_conv_model(model, inputs):
@@ -25,3 +25,17 @@ def get_graph_module_for_llm(model, inputs):
     tracer = torch.fx.Tracer()
     graph = tracer.trace(wrapper_model, concrete_args=bert_inputs)
     return torch.fx.GraphModule(wrapper_model, graph)
+
+
+model_suite = {
+    "resnet18":{
+        "model": torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True, verbose=False).eval(),
+        "inputs": [torch.zeros(1, 3, 224, 224)],
+        "get_graph_module": get_graph_module_for_conv_model,
+    },
+    "bert-base-uncased": {
+        "model": BertForPreTraining.from_pretrained("bert-base-uncased", ignore_mismatched_sizes=True).eval(),
+        "inputs": AutoTokenizer.from_pretrained("bert-base-uncased")("Hello, my dog is cute", padding='max_length', return_tensors="pt")['input_ids'],
+        "get_graph_module": get_graph_module_for_llm,
+    },
+}
