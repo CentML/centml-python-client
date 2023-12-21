1f492ac4f6607f297d5cf4492d003f5e7777c5cc
Anand J <anandj@centml.ai>
2023-12-21 09:18:48 +0000

    Add get, deploy, delete, status commands

--- centml/cli/config.py
+++ centml/cli/config.py
@@ -5,3 +5,5 @@ class Config:
     centml_web_url = "https://pr-178.d193t9o092mgh6.amplifyapp.com/"
     centml_config_dir = os.getenv("CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml"))
     centml_cred_file = centml_config_dir + "/" + os.getenv("CENTML_CRED_FILE", default="credential")
+
+    platformapi_url = "http://3.83.219.28:16000"
