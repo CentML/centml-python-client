a1857321b4221f725234c380b4cb9a54bf0aabee
Anand J <anandj@centml.ai>
2023-12-21 07:26:11 +0000

    Add cluster args

---
+++ centml/cli/cluster.py
@@ -0,0 +1,12 @@
+import platformapi_client
+
+
+def run(cluster_args):
+    if cluster_args.cmd == "ls":
+        pass
+    elif cluster_args.cmd == "get":
+        pass
+    elif cluster_args.cmd == "deploy":
+        pass
+    elif cluster_args.cmd == "status":
+        pass
