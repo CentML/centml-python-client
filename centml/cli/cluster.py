19649598fbcd84b7dece2d49bca6ebff9ae9372b
Anand J <anandj@centml.ai>
2024-02-24 00:01:24 -0500

    ccluster get with service status

--- centml/cli/cluster.py
+++ centml/cli/cluster.py
@@ -2,6 +2,8 @@ import contextlib
 import click
 from tabulate import tabulate
 import platform_api_client
+from platform_api_client.models.endpoint_ready_state import EndpointReadyState
+from platform_api_client.models.deployment_status import DeploymentStatus
 
 from . import login
 from .config import Config
@@ -15,6 +17,22 @@ def get_hw(id):
             return "medium"
         case 1002:
             return "large"
+
+
+def get_ready_status(api_status, service_status):
+    match (api_status, service_status):
+        case (DeploymentStatus.PAUSED, _):
+            return click.style("paused", fg="yellow")
+        case (DeploymentStatus.DELETED, _):
+            return click.style("deleted", fg="white")
+        case (DeploymentStatus.FAILED, _):
+            return click.style("failed", fg="red")
+        case (DeploymentStatus.ACTIVE, EndpointReadyState.NUMBER_1):
+            return click.style("ready", fg="green")
+        case (DeploymentStatus.ACTIVE, EndpointReadyState.NUMBER_2):
+            return click.style("starting", fg="cyan")
+        case (_, _):
+            return click.style("unknown", fg="black", bg="white")
 
 
 @contextlib.contextmanager
@@ -32,7 +50,8 @@ def get_api():
 @click.command(help="List all deployments")
 def ls():
     with get_api() as api:
-        deployments = sorted(api.get_deployments_deployments_get().results, reverse=True, key=lambda d: d.created_at)
+        results = api.get_deployments_deployments_get().results
+        deployments = sorted(results, reverse=True, key=lambda d: d.created_at)
 
         rows = [
             [d.id, d.name, d.type.value, d.status.value, d.created_at.strftime("%Y-%m-%d %H:%M:%S")]
@@ -56,8 +75,10 @@ def get(type, id):
     with get_api() as api:
         if type == "inference":
             deployment = api.get_inference_deployment_deployments_inference_deployment_id_get(id)
+            state = api.get_deployment_status_deployments_status_deployment_id_get(id)
+            ready_status = get_ready_status(deployment.status, state.service_status)
 
-            click.echo(f"Inference deployment #{id} is {deployment.status.value}")
+            click.echo(f"Inference deployment #{id} is {ready_status}")
             click.echo(
                 tabulate(
                     [
