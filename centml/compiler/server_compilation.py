57203015db443d7278fea95a1b3af998f24b5a5b
destefy <stefan.delasa@gmail.com>
2024-03-12 18:59:17 -0400

    use tracing for get_graph_module

--- centml/compiler/server_compilation.py
+++ centml/compiler/server_compilation.py
@@ -3,7 +3,7 @@ import shutil
 import logging
 from typing import List
 import torch
-from torch.fx import GraphModule, Graph
+from torch.fx import GraphModule, Graph, Tracer
 from hidet.graph.frontend import from_torch
 from hidet.graph.frontend.torch.interpreter import Interpreter
 from hidet.graph.frontend.torch.dynamo_backends import (
@@ -13,6 +13,7 @@ from hidet.graph.frontend.torch.dynamo_backends import (
     CompiledForwardFunction,
 )
 from centml.compiler.config import config_instance
+import types
 import pickle
 
 storage_path = os.path.join(config_instance.CACHE_PATH, "server")
@@ -20,28 +21,33 @@ os.makedirs(storage_path, exist_ok=True)
 
 logger = logging.getLogger(__name__)
 
+class CustomTracer(torch.fx.Tracer):
+#     # def __init__(self, root):
+#     #     super().__init__()
+#     #     self.root = root
+    
+    def is_leaf_module(self, m, module_qualified_name):
+        print("CALLED")
+        if isinstance(m, CompiledForwardFunction):
+            return True
+        return super().is_leaf_module(m, module_qualified_name)
+
+class MyModule(torch.nn.Module):
+    def __init__(self, cff):
+        super().__init__()
+        self.leaf_module = cff
+    
+    def forward(self, x):
+        return self.leaf_module(x)
 
 # Create a torch.fx.GraphModule that wraps around `callable`
 # graph_module(*inputs) will call callable.__call__(*inputs)
 # In this function, `example_inputs`` is only used to determine the number of input nodes
 def get_graph_module(callable, example_inputs):
-    graph = Graph()
-    input_nodes = [
-        graph.create_node(op='placeholder', target=f'input_{i}', args=(), kwargs={}) for i in range(len(example_inputs))
-    ]
-    
-    # Create a `get_attr` node that refers to `callable`
-    callable_node = graph.create_node(op='get_attr', target='callable')
-
-    # This should call "__call__" method of `callable` (the root)
-    function_node = graph.create_node(op='call_method', target="__call__", args=(callable_node, *input_nodes), kwargs={})
-    graph.output(function_node)
-    
-    # Make `callable` an attribute of a new parent module
-    parent_module = torch.nn.Module()
-    parent_module.callable = callable
-
-    graph_module = GraphModule(parent_module, graph)
+    module = MyModule(callable)
+    tracer = CustomTracer()
+    graph = tracer.trace(module, example_inputs)
+    graph_module = GraphModule(module, graph)
 
     return graph_module
 
@@ -60,7 +66,6 @@ def dir_cleanup(model_id: str):
     except Exception as e:
         raise Exception("Failed to delete the directory") from e
 
-
 def hidet_backend_server(graph_module: GraphModule, example_inputs: List[torch.Tensor], model_id: str):
     assert isinstance(graph_module, GraphModule)
 
@@ -77,13 +82,13 @@ def hidet_backend_server(graph_module: GraphModule, example_inputs: List[torch.T
     cgraph.run_async(hidet_inputs)
 
     # Get compiled forward function
-    wrapper = CompiledForwardFunction(cgraph, example_inputs, output_format)
-
+    wrapper = CompiledForwardFunction(cgraph, hidet_inputs, output_format)
+    
     # Wrap the forward function in a torch.fx.GraphModule
     graph_module = get_graph_module(wrapper, example_inputs)
-    
+
     graph_module.graph.print_tabular()
-    
+
     try:
         # This uses pickle to serialize to disk
         with open(os.path.join(storage_path, model_id, "graph_module.zip"), "wb") as f:
