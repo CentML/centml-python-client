4c7db028bf0646d69f37b3831d9958c7c294ac58
Stefan de Lasa <stefan.delasa@centml.ai>
2024-04-05 16:48:02 -0400

    return RootRCModule instead of GraphModule

--- centml/compiler/server_compilation.py
+++ centml/compiler/server_compilation.py
@@ -1,8 +1,9 @@
 import os
+import pickle
 import shutil
 import logging
-from typing import List
-import pickle
+from enum import Enum
+from typing import List, Callable
 import torch
 from torch.fx import GraphModule
 from hidet.graph.frontend import from_torch
@@ -21,48 +22,6 @@ os.makedirs(storage_path, exist_ok=True)
 logger = logging.getLogger(__name__)
 
 
-# Calling the torch.fx.GraphModule will call RootModule.forward
-# Due to limitations on how the GraphModule is constructed, args are passed as a tuple and later unpacked
-class RootModule(torch.nn.Module):
-    def __init__(self, callable):
-        super().__init__()
-        self.leaf_module = callable
-
-    def forward(self, args):
-        return self.leaf_module(args)
-
-
-# We wrap the callable in a torch.nn.Module so that it can be a leaf module in the graph
-# Leaf modules avoid being traced by the torch.fx.Tracer and are treated like black boxes
-class ModuleWrapper(torch.nn.Module):
-    def __init__(self, callable):
-        super().__init__()
-        self.callable = callable
-
-    def forward(self, args):
-        return self.callable(*args)
-
-
-# Note: pickling a GraphModule won't save its Graph.
-# Instead, it stores the Tracer and re-traces to recreate the Graph when deserializing.
-class CustomTracer(torch.fx.Tracer):
-    # Don't trace the ModuleWrapper
-    def is_leaf_module(self, m, module_qualified_name):
-        if isinstance(m, ModuleWrapper):
-            return True
-        return super().is_leaf_module(m, module_qualified_name)
-
-
-# Create a torch.fx.GraphModule that wraps around `callable`.
-# `callable` is a class whose forward function gets called when we call the GraphModule's forward function
-def get_graph_module(callable):
-    module = ModuleWrapper(callable)
-    root = RootModule(module)
-    tracer = CustomTracer()
-    graph = tracer.trace(root)
-    return GraphModule(root, graph)
-
-
 # This function will delete the storage_path/{model_id} directory
 def dir_cleanup(model_id: str):
     dir_path = os.path.join(storage_path, model_id)
@@ -76,6 +35,28 @@ def dir_cleanup(model_id: str):
         shutil.rmtree(dir_path)
     except Exception as e:
         raise Exception("Failed to delete the directory") from e
+
+
+class CompilerType(Enum):
+    HIDET = "hidet"
+
+
+class RootRCModule(Callable):
+    def __init__(self, compiler_name: CompilerType):
+        self.compiler_name = compiler_name
+
+    # Implement in child class
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class HidetRCModule(RootRCModule):
+    def __init__(self, hidet_compiled_model):
+        super().__init__(CompilerType.HIDET)
+        self.compiled_model_forward = hidet_compiled_model
+
+    def __call__(self, *args, **kwargs):
+        return self.compiled_model_forward(*args)
 
 
 def hidet_backend_server(input_graph_module: GraphModule, example_inputs: List[torch.Tensor], model_id: str):
@@ -94,7 +75,7 @@ def hidet_backend_server(input_graph_module: GraphModule, example_inputs: List[t
     wrapper = HidetCompiledModel(cgraph, hidet_inputs, output_format)
 
     # Wrap the forward function in a torch.fx.GraphModule
-    compiled_graph_module = get_graph_module(wrapper)
+    compiled_graph_module = HidetRCModule(wrapper)
 
     try:
         with open(os.path.join(storage_path, model_id, "graph_module.zip"), "wb") as f:
