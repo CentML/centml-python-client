b7fa38f76394923aa809d6c4dfcac0c9721a32d7
destefy <stefan.delasa@gmail.com>
2024-04-10 12:45:35 -0400

    Add constants to config

--- centml/compiler/config.py
+++ centml/compiler/config.py
@@ -16,6 +16,10 @@ class Config:
     CACHE_PATH = os.getenv("CENTML_CACHE_DIR", default=os.path.expanduser("~/.cache/centml"))
     SERVER_IP = os.getenv("CENTML_SERVER_IP", default="0.0.0.0")
     SERVER_PORT = os.getenv("CENTML_SERVER_PORT", default="8080")
+    SERVER_URL = f"http://{SERVER_IP}:{SERVER_PORT}"
+
+    BACKEND_BASE_PATH = os.path.join(CACHE_PATH, "backend")
+    SERVER_BASE_PATH = os.path.join(CACHE_PATH, "server")
 
 
 config_instance = Config()
