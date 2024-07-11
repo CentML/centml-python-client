94d10c3ab9bbe05bfee532dbbb6328a4d00cd171
destefy <stefan.delasa@gmail.com>
2024-07-11 14:04:30 -0400

    use pydantic BaseSettings

--- centml/sdk/config.py
+++ centml/sdk/config.py
@@ -1,11 +1,12 @@
 import os
+from pydantic_settings import BaseSettings
 
 
-class Config:
-    centml_web_url = "https://main.d1tz9z8hgabab9.amplifyapp.com/"
-    centml_config_dir = os.getenv("CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml"))
-    centml_cred_file = centml_config_dir + "/" + os.getenv("CENTML_CRED_FILE", default="credential")
+class Config(BaseSettings):
+    centml_web_url: str = "https://main.d1tz9z8hgabab9.amplifyapp.com/"
+    centml_config_dir: str = os.getenv("CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml"))
+    centml_cred_file: str = centml_config_dir + "/" + os.getenv("CENTML_CRED_FILE", default="credential")
 
-    platformapi_url = "https://api.centml.org"
+    platformapi_url: str = "https://api.centml.org"
 
-    firebase_api_key = "AIzaSyBXSNjruNdtypqUt_CPhB8QNl8Djfh5RXI"
+    firebase_api_key: str = "AIzaSyBXSNjruNdtypqUt_CPhB8QNl8Djfh5RXI"
