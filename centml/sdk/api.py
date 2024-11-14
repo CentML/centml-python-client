25e3b66081a9f369faacb428e2865ba6febbbfa2
Michael Shin <michaelshin@users.noreply.github.com>
2024-11-14 10:26:37 -0500

    Updated cluster commands to use platform client 0.3.1 (#80)
    * Updated cluster commands to use platform client 0.3.1

--- centml/sdk/api.py
+++ centml/sdk/api.py
@@ -3,8 +3,8 @@ from contextlib import contextmanager
 import platform_api_python_client
 from platform_api_python_client import (
     DeploymentStatus,
-    CreateInferenceDeploymentV2Request,
-    CreateComputeDeploymentV2Request,
+    CreateInferenceDeploymentRequest,
+    CreateComputeDeploymentRequest,
     CreateCServeDeploymentRequest,
 )
 
@@ -14,37 +14,37 @@ from centml.sdk.config import settings
 
 class CentMLClient:
     def __init__(self, api):
-        self._api = api
+        self._api: platform_api_python_client.EXTERNALApi = api
 
     def get(self, depl_type):
-        results = self._api.get_deployments_deployments_v2_get(type=depl_type).results
+        results = self._api.get_deployments_deployments_get(type=depl_type).results
         deployments = sorted(results, reverse=True, key=lambda d: d.created_at)
         return deployments
 
     def get_status(self, id):
-        return self._api.get_deployment_status_deployments_v2_status_deployment_id_get(id)
+        return self._api.get_deployment_status_deployments_status_deployment_id_get(id)
 
     def get_inference(self, id):
-        return self._api.get_inference_deployment_deployments_v2_inference_deployment_id_get(id)
+        return self._api.get_inference_deployment_deployments_inference_deployment_id_get(id)
 
     def get_compute(self, id):
-        return self._api.get_compute_deployment_deployments_v2_compute_deployment_id_get(id)
+        return self._api.get_compute_deployment_deployments_compute_deployment_id_get(id)
 
     def get_cserve(self, id):
-        return self._api.get_cserve_deployment_deployments_v2_cserve_deployment_id_get(id)
+        return self._api.get_cserve_deployment_deployments_cserve_deployment_id_get(id)
 
-    def create_inference(self, request: CreateInferenceDeploymentV2Request):
-        return self._api.create_inference_deployment_deployments_v2_inference_post(request)
+    def create_inference(self, request: CreateInferenceDeploymentRequest):
+        return self._api.create_inference_deployment_deployments_inference_post(request)
 
-    def create_compute(self, request: CreateComputeDeploymentV2Request):
+    def create_compute(self, request: CreateComputeDeploymentRequest):
         return self._api.create_compute_deployment_deployments_compute_post(request)
 
     def create_cserve(self, request: CreateCServeDeploymentRequest):
-        return self._api.create_cserve_deployment_deployments_v2_cserve_post(request)
+        return self._api.create_cserve_deployment_deployments_cserve_post(request)
 
     def _update_status(self, id, new_status):
         status_req = platform_api_python_client.DeploymentStatusRequest(status=new_status)
-        self._api.update_deployment_status_deployments_v2_status_deployment_id_put(id, status_req)
+        self._api.update_deployment_status_deployments_status_deployment_id_put(id, status_req)
 
     def delete(self, id):
         self._update_status(id, DeploymentStatus.DELETED)
@@ -59,7 +59,7 @@ class CentMLClient:
         return self._api.get_clusters_clusters_get()
 
     def get_hardware_instances(self, cluster_id):
-        return self._api.get_hardware_instances_hardware_instances_v2_get(cluster_id).results
+        return self._api.get_hardware_instances_hardware_instances_get(cluster_id).results
 
 
 @contextmanager
--- centml/sdk/config.py
+++ centml/sdk/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     CENTML_WEB_URL: str = "https://app.centml.com/"
     CENTML_CONFIG_PATH: str = os.path.expanduser("~/.centml")
-    CENTML_CRED_FILE: str = "credential"
+    CENTML_CRED_FILE: str = "credentials.json"
     CENTML_CRED_FILE_PATH: str = CENTML_CONFIG_PATH + "/" + CENTML_CRED_FILE
 
     CENTML_PLATFORM_API_URL: str = "https://api.centml.com"
