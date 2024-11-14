46e2af2f47672e3b95b55dafe7b5bcbadca1139f
Michael Shin <michaelshin@users.noreply.github.com>
2024-11-14 12:56:30 -0500

    Updated package version to 0.3.0 in setup.py (#81)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='centml',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     python_requires="<3.12",
     long_description=open('README.md').read(),
