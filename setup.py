ed8ecf1881fec701eab1b67af652172e2d0f5f2e
Honglin Cao <honglin.cao@centml.ai>
2024-12-19 13:09:05 -0500

    sync version to 3.1.4, the same as platform and api client

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='centml',
-    version='0.3.2',
+    version='3.1.4',
     packages=find_packages(),
     python_requires=">=3.10",
     long_description=open('README.md').read(),
