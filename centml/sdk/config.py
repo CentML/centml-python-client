7053b663ba35b1de56914946212fee3f958ac68b
John Calderon <john.calderon@centml.ai>
2025-01-27 11:33:52 -0500

    fix lint

--- centml/sdk/config.py
+++ centml/sdk/config.py
@@ -11,10 +11,11 @@ class Config(BaseSettings):
     CENTML_WEB_URL: str = os.getenv("CENTML_WEB_URL") or "https://app.centml.com/"
     CENTML_CONFIG_PATH: str = os.getenv("CENTML_CONFIG_PATH") or os.path.expanduser("~/.centml")
     CENTML_CRED_FILE: str = os.getenv("CENTML_CRED_FILE") or "credentials.json"
-    CENTML_CRED_FILE_PATH: str = os.path.join(CENTML_CONFIG_PATH,CENTML_CRED_FILE)
+    CENTML_CRED_FILE_PATH: str = os.path.join(CENTML_CONFIG_PATH, CENTML_CRED_FILE)
 
     CENTML_PLATFORM_API_URL: str = os.getenv("CENTML_PLATFORM_API_URL") or "https://api.centml.com"
 
-    CENTML_FIREBASE_API_KEY: str = os.getenv("CENTML_FIREBASE_API_KEY") or "AIzaSyChPXy41cIAxS_Nd8oaYKyP_oKkIucobtY" 
+    CENTML_FIREBASE_API_KEY: str = os.getenv("CENTML_FIREBASE_API_KEY") or "AIzaSyChPXy41cIAxS_Nd8oaYKyP_oKkIucobtY"
+
 
 settings = Config()
