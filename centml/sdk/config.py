9443cee5f25c2c84dfc203b5198f64634c2ca0f2
John Calderon <81483067+johncalesp@users.noreply.github.com>
2025-02-25 11:41:30 -0500

    [ENV VARS] Read env vars from user input (#86)
    * change env var
    
    * remove jump line
    
    * fix lint
    
    * add default values
    
    * fix lint
    
    ---------
    
    Co-authored-by: John Calderon <john.calderon@centml.ai>

--- centml/sdk/config.py
+++ centml/sdk/config.py
@@ -8,14 +8,16 @@ class Config(BaseSettings):
     # It is possible to override the default values by setting the environment variables
     model_config = SettingsConfigDict(env_file=Path('.env'))
 
-    CENTML_WEB_URL: str = "https://app.centml.com/"
-    CENTML_CONFIG_PATH: str = os.path.expanduser("~/.centml")
-    CENTML_CRED_FILE: str = "credentials.json"
-    CENTML_CRED_FILE_PATH: str = CENTML_CONFIG_PATH + "/" + CENTML_CRED_FILE
+    CENTML_WEB_URL: str = os.getenv("CENTML_WEB_URL", default="https://app.centml.com/")
+    CENTML_CONFIG_PATH: str = os.getenv("CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml"))
+    CENTML_CRED_FILE: str = os.getenv("CENTML_CRED_FILE", default="credentials.json")
+    CENTML_CRED_FILE_PATH: str = os.path.join(CENTML_CONFIG_PATH, CENTML_CRED_FILE)
 
-    CENTML_PLATFORM_API_URL: str = "https://api.centml.com"
+    CENTML_PLATFORM_API_URL: str = os.getenv("CENTML_PLATFORM_API_URL", default="https://api.centml.com")
 
-    CENTML_FIREBASE_API_KEY: str = "AIzaSyChPXy41cIAxS_Nd8oaYKyP_oKkIucobtY"
+    CENTML_FIREBASE_API_KEY: str = os.getenv(
+        "CENTML_FIREBASE_API_KEY", default="AIzaSyChPXy41cIAxS_Nd8oaYKyP_oKkIucobtY"
+    )
 
 
 settings = Config()
