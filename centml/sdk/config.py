059bb0ca2171e9ae2f43098356b748608cafaab8
Honglin Cao <h45cao@uwaterloo.ca>
2025-09-02 09:57:28 -0400

    black format

--- centml/sdk/config.py
+++ centml/sdk/config.py
@@ -8,19 +8,13 @@ class Config(BaseSettings):
     model_config = SettingsConfigDict(env_file=Path(".env"))
 
     CENTML_WEB_URL: str = os.getenv("CENTML_WEB_URL", default="https://app.centml.com/")
-    CENTML_CONFIG_PATH: str = os.getenv(
-        "CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml")
-    )
+    CENTML_CONFIG_PATH: str = os.getenv("CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml"))
     CENTML_CRED_FILE: str = os.getenv("CENTML_CRED_FILE", default="credentials.json")
     CENTML_CRED_FILE_PATH: str = os.path.join(CENTML_CONFIG_PATH, CENTML_CRED_FILE)
 
-    CENTML_PLATFORM_API_URL: str = os.getenv(
-        "CENTML_PLATFORM_API_URL", default="https://api.centml.com"
-    )
+    CENTML_PLATFORM_API_URL: str = os.getenv("CENTML_PLATFORM_API_URL", default="https://api.centml.com")
 
-    CENTML_WORKOS_CLIENT_ID: str = os.getenv(
-        "CENTML_WORKOS_CLIENT_ID", default="client_01JP5TWW2997MF8AYQXHJEGYR0"
-    )
+    CENTML_WORKOS_CLIENT_ID: str = os.getenv("CENTML_WORKOS_CLIENT_ID", default="client_01JP5TWW2997MF8AYQXHJEGYR0")
 
 
 settings = Config()
