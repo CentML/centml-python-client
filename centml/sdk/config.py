5fdcae8c2f3444f4c1523c4de95d7cd6605838f3
Honglin Cao <Caohonglin317@hotmail.com>
2025-10-01 15:04:35 -0400

    TEMPL change to local env for now

--- centml/sdk/config.py
+++ centml/sdk/config.py
@@ -8,14 +8,14 @@ class Config(BaseSettings):
     # It is possible to override the default values by setting the environment variables
     model_config = SettingsConfigDict(env_file=Path(".env"))
 
-    CENTML_WEB_URL: str = os.getenv("CENTML_WEB_URL", default="https://app.centml.com/")
+    CENTML_WEB_URL: str = os.getenv("CENTML_WEB_URL", default="http://localhost:5173")
     CENTML_CONFIG_PATH: str = os.getenv("CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml"))
     CENTML_CRED_FILE: str = os.getenv("CENTML_CRED_FILE", default="credentials.json")
     CENTML_CRED_FILE_PATH: str = os.path.join(CENTML_CONFIG_PATH, CENTML_CRED_FILE)
 
-    CENTML_PLATFORM_API_URL: str = os.getenv("CENTML_PLATFORM_API_URL", default="https://api.centml.com")
+    CENTML_PLATFORM_API_URL: str = os.getenv("CENTML_PLATFORM_API_URL", default="http://localhost:16000")
 
-    CENTML_WORKOS_CLIENT_ID: str = os.getenv("CENTML_WORKOS_CLIENT_ID", default="client_01JP5TWW2997MF8AYQXHJEGYR0")
+    CENTML_WORKOS_CLIENT_ID: str = os.getenv("CENTML_WORKOS_CLIENT_ID", default="client_01JP5TWVNBMQ5FVC777FQZC661")
 
     # Long-term credentials - can be set via environment variables
     CENTML_SERVICE_ACCOUNT_SECRET: Optional[str] = os.getenv("CENTML_SERVICE_ACCOUNT_SECRET", default=None)
