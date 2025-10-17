37b5d00babb11b5add0624174a557e93b4c2a7b5
JoudChataoui <70609706+JoudChataoui@users.noreply.github.com>
2025-10-17 10:14:51 -0400

    Allow extra env variables when creating config (#114)

--- centml/sdk/config.py
+++ centml/sdk/config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Config(BaseSettings):
     # It is possible to override the default values by setting the environment variables
-    model_config = SettingsConfigDict(env_file=Path(".env"))
+    model_config = SettingsConfigDict(env_file=Path(".env"), extra="ignore")
 
     CENTML_WEB_URL: str = os.getenv("CENTML_WEB_URL", default="https://app.centml.com/")
     CENTML_CONFIG_PATH: str = os.getenv("CENTML_CONFIG_PATH", default=os.path.expanduser("~/.centml"))
